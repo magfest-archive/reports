024d9860fbc16c1a062320f222b61453e106eed1
Dominic Cerquetti <binary1230@gmail.com>
2016-09-15 23:49:11 -0400

    guard against not having 'c' object, versions pre-'c' refactor lack this

--- reports/config.py
+++ reports/config.py
@@ -1,7 +1,10 @@
 from uber.common import *
-
 from uber._version import __version__
+
 if float(__version__) > 2014.09:
     # magfest 8.5 compatibility
     config = parse_config(__file__)
+
+if float(__version__) > 2015.01:
+    # didn't have 'c' object yet
     c.include_plugin_config(config)
