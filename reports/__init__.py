afe295816b92a99edab8158964bee1b383db6715
Dominic Cerquetti <binary1230@gmail.com>
2016-09-16 22:46:55 -0400

    m8.5: implement backwards compatibiltiy with older config system - hack hack hack.  we'll never use this code again. i am not proud.

--- reports/__init__.py
+++ reports/__init__.py
@@ -6,7 +6,16 @@ from .model_checks import *
 from .automated_emails import *
 
 from uber._version import __version__
+
+if float(__version__) > 2014.09:
+    # anything beyond magfest 8.5 can access this normally
+    module_root = config['module_root']
+else:
+    # magfest 8.5 doesn't have new-style config stuff, so fake it.
+    # new code should NEVER get here.
+    module_root = '/usr/local/uber85/plugins/reports/reports'
+
 if float(__version__) >= 2014.09:
-    static_overrides(join(config['module_root'], 'static'))
-    template_overrides(join(config['module_root'], 'templates'))
-    mount_site_sections(config['module_root'])
+    static_overrides(join(module_root, 'static'))
+    template_overrides(join(module_root, 'templates'))
+    mount_site_sections(module_root)
