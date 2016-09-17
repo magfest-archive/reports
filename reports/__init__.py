6b1811de5e027c6f15cfbcfe820f75adca3883d6
Dominic Cerquetti <binary1230@gmail.com>
2016-09-16 21:22:00 -0400

    unguard magfest 8.5 compatability, this _should_ work ok now

--- reports/__init__.py
+++ reports/__init__.py
@@ -5,11 +5,8 @@ from .models import *
 from .model_checks import *
 from .automated_emails import *
 
-# magfest 8.5 compatibility
 from uber._version import __version__
-if float(__version__) > 2014.09:
-    # TODO: we'll need to figure out how to do this stuff for mag8.5 which
-    # is pre-sideboard. might still be easy.
+if float(__version__) >= 2014.09:
     static_overrides(join(config['module_root'], 'static'))
     template_overrides(join(config['module_root'], 'templates'))
     mount_site_sections(config['module_root'])
